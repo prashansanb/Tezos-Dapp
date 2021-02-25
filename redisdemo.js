0d5016f08ef96189f365cf720a726d463264b4e3
Prashansa Bhattacharyya <34596426+prashansanb@users.noreply.github.com>
2021-02-25 20:33:51 +0530

    Update redisdemo.js

--- redisdemo.js
+++ redisdemo.js
@@ -1,20 +1,10 @@
 
-import TezosToolkit  from '@taquito/taquito';
-import InMemorySigner from '@taquito/signer';
 import importKey from '@taquito/signer';
 const express = require("express");
 const fetch = require("node-fetch");
 const axios = require("axios");
 const redis = require("redis");
 const app = express();
-
-const Tezos = new TezosToolkit('https://api.tez.ie/rpc/delphinet');
-Tezos.tz
-  .getBalance('tz1Zq86VbqKKjgjNeaKUXfBTmgMfPjWgeTnK')
-  .then((balance) => println(`${balance.toNumber() / 1000000} ꜩ`))
-  .catch((error) => println(JSON.stringify(error)));
-
-
 
 const redisPort = 6379
 const client = redis.createClient(redisPort);
@@ -31,7 +21,7 @@ app.get('/fetchdata', (req, res) => {
 app.get('/photos', (req, res) => {
     const photosRedisKey = 'user:photos';
  
-    // Try fetching the result from Redis first in case we have it cached
+    ///Incase cached
     return client.get(photosRedisKey, (err, photos) => {
  
         // If that key exists in Redis store
@@ -39,14 +29,13 @@ app.get('/photos', (req, res) => {
  
             return res.json({ source: 'cache', data: JSON.parse(photos) })
  
-        } else { // Key does not exist in Redis store
- 
+        } else { 
             // Fetch directly from remote api
             fetch('https://jsonplaceholder.typicode.com/photos')
                 .then(response => response.json())
                 .then(photos => {
  
-                    // Save the  API response in Redis store,  data expire time in 3600 seconds, it means one hour
+                    // Save the  API response in Redis store,  data expire time in 3600 seconds
                     client.setex(photosRedisKey, 3600, JSON.stringify(photos))
  
                     // Send JSON response to client
@@ -54,9 +43,7 @@ app.get('/photos', (req, res) => {
  
                 })
                 .catch(error => {
-                    // log error message
                     console.log(error)
-                    // send error to the client 
                     return res.json(error.toString())
                 })
         }
