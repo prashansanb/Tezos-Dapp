2b12801ac6ecd87c6bc7dcd5ffd2d86749d95f7b
Prashansa <prashansa.bhattacharyya@gmail.com>
2021-02-26 18:22:01 +0530

    integrated components

--- src/app.ts
+++ src/app.ts
@@ -1,46 +1,77 @@
 
 import express from 'express';
-//import { TezosToolkit } from '@taquito/taquito';
-//import { TezBridgeSigner } from '@taquito/tezbridge-signer';
-
+var taquito = require("@taquito/taquito")
+const redis = require("redis");
 const axios = require('axios');
+const fetch = require("node-fetch");
 const app = express();
 const port = 3000;
 
-//const tezos = new TezosToolkit('https://delphinet.smartpy.io');
-//tezos.setProvider({ signer: new TezBridgeSigner() });
-//tezos.tz.getBalance('edskRs3ouFyhyzrddNyFSy51MzJaqDfQSVtPGzsBC7aSqDcrxWuhEsyG345N9e3eirtSRobnCgBJ5Wj1NoJWxSjJqHp5JmBfQ9').then((balance) => console.log(`${balance.toNumber() / 1000000} ꜩ`)).catch((error) => console.log(JSON.stringify(error)));
-// Tezos.tz
-//   .getBalance('edskRs3ouFyhyzrddNyFSy51MzJaqDfQSVtPGzsBC7aSqDcrxWuhEsyG345N9e3eirtSRobnCgBJ5Wj1NoJWxSjJqHp5JmBfQ9')
-//   .then((balance) => console.log(`${balance.toNumber() / 1000000} ꜩ`))
-//   .catch((error) => console.log(JSON.stringify(error)));
 
+const tezos = new taquito.TezosToolkit('https://delphinet.smartpy.io');
 
-app.get('/', (req, res) => {
-    res.send('API WORKING');
-
-});
+const redisPort = 6379
+const client = redis.createClient(redisPort);
+client.on("error", (err) => {
+    console.log(err);
+})
 
 async function fetchfrombchain() {
+    console.log('fetchfrombchain indexer called');
     let res = await axios.get('https://better-call.dev/v1/bigmap/delphinet/75796/keys');
     let data = res.data;
-    //console.log(data);
-    for(var i in data){
+    for (var i in data) {
         var objectInstance = data[i].data;
-        var timest= data[i].timestamp;
-        for(var j in objectInstance.value.children) {
-            var account = objectInstance.value.children[j].value;
-            console.log(account);
+
+        for (var j in objectInstance.value.children) {
+            var vault = objectInstance.value.children[j].value;
+            const RedisKey = vault;
+            client.get(RedisKey, (err, address) => {
+
+                if (address) {
+                    console.log('exists in cache');
+                }
+                else {
+                    tezos.tz
+                        .getBalance(RedisKey)
+                        .then((balance) => {
+                            console.log(`${balance.toNumber() / 1000000} ꜩ`);
+                            //client.set(RedisKey, JSON.stringify(balance))
+                            client.set(RedisKey, balance.toNumber());
+                        })
+                }
+            })
+
         }
-        console.log('...');
     }
-    
 }
+
 app.get('/fetchindexdata', (req, res) => {
     fetchfrombchain();
-    res.send('INDEX DATA');
+    res.send('USER DETAILS');
 });
+app.get('/getinfo', (req, res) => {
+    let address = 'KT1BfUUUPFmGJsMLMAjo2AfJ7UbcmqDqEF2k';
+    console.log('user details');
 
+    client.get(address, (err, value) => {
+
+        if (value) {
+            console.log('exists in cache');
+            res.send(value);
+        }
+        else {
+            tezos.tz
+                .getBalance(address)
+                .then((balance) => {
+                    console.log(`${balance.toNumber() / 1000000} ꜩ`);
+                    //client.set(RedisKey, JSON.stringify(balance))
+                    client.set(address, balance.toNumber());
+
+                })
+        }
+    })
+});
 app.listen(port, () => {
     console.log("Node server started");
 });
