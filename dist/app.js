5f96ffcd3530e2045e8337bdd8dc873868dba0bc
Prashansa <prashansa.bhattacharyya@gmail.com>
2021-02-26 14:34:06 +0530

    Fetched accounts

--- dist/app.js
+++ dist/app.js
@@ -12,6 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+//import { TezosToolkit } from '@taquito/taquito';
+//import { TezBridgeSigner } from '@taquito/tezbridge-signer';
 const axios = require('axios');
 const app = express_1.default();
 const port = 3000;
