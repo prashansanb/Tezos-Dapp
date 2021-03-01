ca6d15c7dadaed5a0e7edd3cb2c6f2b8186db9bf
Prashansa <prashansa.bhattacharyya@gmail.com>
2021-03-01 16:34:19 +0530

    api completed

--- dist/app.js
+++ dist/app.js
@@ -35,42 +35,6 @@ const client = redis.createClient(redisPort);
 client.on("error", (err) => {
     console.log(err);
 });
-function fetchfrombchain() {
-    return __awaiter(this, void 0, void 0, function* () {
-        console.log('fetchfrombchain indexer called');
-        let res = yield axios.get('https://better-call.dev/v1/bigmap/delphinet/75796/keys');
-        let data = res.data;
-        for (var i in data) {
-            var objectInstance = data[i].data;
-            var accountowner = objectInstance.key.value;
-            for (var j in objectInstance.value.children) {
-                var vault = objectInstance.value.children[j].value;
-                console.log(vault);
-                const contract = yield tezos.contract.at(vault);
-                var storage = yield contract.storage();
-                const RedisKey = vault;
-                try {
-                }
-                catch (error) {
-                }
-                client.setex(RedisKey, 3600, JSON.stringify({
-                    Insurance: Date.parse(storage.Insurance),
-                    duration: Date.parse(storage.duration),
-                    Liquidated: String(storage.Liquidated),
-                    interest: storage.interest.toNumber(),
-                    interestRate: storage.interestRate.toNumber(),
-                    owner: String(storage.owner),
-                    securityDelegator: String(storage.securityDelegator),
-                    token: storage.token.toNumber(),
-                    xtz: storage.xtz.toNumber()
-                }, () => {
-                    //write error
-                }));
-            }
-        }
-        return res.status(200);
-    });
-}
 app.get('/fetchindexdata', (req, res) => __awaiter(this, void 0, void 0, function* () {
     let result = yield axios.get('https://better-call.dev/v1/bigmap/delphinet/75796/keys');
     let data = result.data;
@@ -95,36 +59,19 @@ function fetchinfo() {
         let address = 'KT1BfUUUPFmGJsMLMAjo2AfJ7UbcmqDqEF2k';
         const contract = yield tezos.contract.at(address);
         var storage = yield contract.storage();
-        try {
-            client.hgetall(address, (err, value) => {
-                if (value) {
-                    console.log('exists in cache');
-                    console.log(value);
-                }
-                else {
-                    //STRINGIFY
-                    const RedisKey = address;
-                    client.hset(RedisKey, JSON.stringify({
-                        'Insurance': Date.parse(storage.Insurance),
-                        'duration': Date.parse(storage.duration),
-                        'Liquidated': String(storage.Liquidated),
-                        'interest': storage.interest.toNumber(),
-                        'interestRate': storage.interestRate.toNumber(),
-                        'owner': String(storage.owner),
-                        'securityDelegator': String(storage.securityDelegator),
-                        'token': storage.token.toNumber(),
-                        'xtz': storage.xtz.toNumber()
-                    }, () => {
-                        //catch error
-                    }));
-                    console.log('stored in cache');
-                }
-            });
-            console.log(JSON.parse(yield client.get(address)));
-        }
-        catch (error) {
-            console.error(error);
-        }
+        client.hgetall(address, (err, value) => {
+            if (value) {
+                console.log('exists in cache');
+                console.log(value);
+            }
+            else {
+                const RedisKey = address;
+                client.hmset(RedisKey, 'Insurance', Date.parse(storage.Insurance), 'duration', Date.parse(storage.duration), 'Liquidated', String(storage.Liquidated), 'interest', storage.interest.toNumber(), 'interestRate', storage.interestRate.toNumber(), 'owner', String(storage.owner), 'securityDelegator', String(storage.securityDelegator), 'token', storage.token.toNumber(), 'xtz', storage.xtz.toNumber(), function (err, reply) {
+                    console.log(reply);
+                });
+                console.log('stored in cache');
+            }
+        });
     });
 }
 app.get('/getinfo', (req, res) => {
